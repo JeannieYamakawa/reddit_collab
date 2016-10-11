57751aef885acbe2a2c5e53438d58bec74134b48
Diego Goding <diego.goding@gmail.com>
2016-10-11 16:12:25 -0500

    users modules

--- migrations/20161011153158_create_posts_table.js
+++ migrations/20161011153158_create_posts_table.js
@@ -1,12 +1,20 @@
-
 exports.up = function(knex, Promise) {
-  return knex.schema.createTableIfNotExists('posts', function (table) {
+  return knex.schema.createTableIfNotExists('posts', function(table) {
     table.increments();
-    table.string('title').notNullable().defaultTo("");
-    table.string('body').notNullable().defaultTo("");
-    table.integer('user_id').unsigned().index().references('id').inTable('users').onDelete('CASCADE');
+    table.string('title')
+      .notNullable()
+      .defaultTo('');
+    table.string('body')
+      .notNullable()
+      .defaultTo('');
+    table.integer('user_id')
+      .unsigned()
+      .index()
+      .references('id')
+      .inTable('users')
+      .onDelete('CASCADE');
     table.timestamps(true, true);
-  })
+  });
 };
 
 exports.down = function(knex, Promise) {
---
+++ modules/reddit_app.js
@@ -0,0 +1,33 @@
+var app = {
+  users: {
+    get: {
+      all: function() {
+        return knex('users')
+          .select(['id', 'username', 'email'])
+          .orderBy('id');
+
+      },
+      byID: function(user_id) {
+        return knex('users')
+          .select(['id', 'username', 'email'])
+          .first()
+          .where({
+            id: user_id
+          });
+      },
+      byUsernameOrEmailWithHash: function(user_login) {
+        return knex('users')
+          .select(['id', 'username', 'email', 'password'])
+          .first()
+          .where({
+            email: user_login
+          })
+          .OrWhere({
+            username: user_login
+          });
+      }
+    }
+  }
+};
+
+moodule.exports = app;
