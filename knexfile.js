1b22b7b2155bfee3b9425fc1ee7c37207a0c91c7
Roberto  Varela <rvarela11@Roberts-MacBook-Pro.local>
2016-10-11 16:13:09 -0500

    fixed merge problem

--- knexfile.js
+++ knexfile.js
@@ -4,24 +4,19 @@
 
 module.exports = {
 
-    development: {
-        client: 'pg',
-        connection: {
-            host: '127.0.0.1',
-            database: 'reddit_collab_dev'
-        }
-    },
-
-    test: {
-        client: 'pg',
-        connection: {
-            host: '127.0.0.1',
-            database: 'reddit_collab_test'
-        }
-    },
-
-    production: {
-        client: 'pg',
-        connection: process.env.DATABASE_URL
+  development: {
+    client: 'pg',
+    connection: {
+      host: '127.0.0.1',
+      database: 'reddit_collab_dev'
     }
+  },
+
+  test: {
+    client: 'pg',
+    connection: {
+      host: '127.0.0.1',
+      database: 'reddit_collab_test'
+    }
+  }
 };
