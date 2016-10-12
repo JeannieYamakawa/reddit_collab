9fcd0b9cc40cc1c437e3ebb60ea32bea0a254dc0
Joseph Tongay <dirk.tongay@gmail.com>
2016-10-12 14:50:39 -0500

    edit page

--- routes/posts.js
+++ routes/posts.js
@@ -14,32 +14,31 @@ function authorizedUser(req, res, next) {
   }
 }
 
-router.get('/', (req, res, next) => {
-  knex('users').innerJoin('posts', 'users.id', 'posts.user_id').then((posts) => {
-    res.render('posts', {
-      posts: posts
-    })
-  })
-})
+router.get( '/', ( req, res, next ) => {
+    knex( 'users' ).innerJoin('posts', 'users.id', 'posts.user_id').then( ( posts ) => {
+        res.render( 'posts', {
+            posts: posts
+        } )
+    } )
+} )
 
-router.get('/:id', (req, res, next) => {
-  let postID = req.params.id;
-  knex('posts').where('posts.id', postID).innerJoin('comments', 'posts.id',
-    'comments.post_id').then((post) => {
-    res.render('single-post', {
-      post: post
-    })
-  })
-})
+router.get( '/:id', ( req, res, next ) => {
+    let postID = req.params.id;
+    knex( 'posts' ).where( 'posts.id', postID ).innerJoin('comments', 'posts.id', 'comments.post_id').then( ( post ) => {
+        res.render( 'single-post', {
+            post: post
+        } )
+    } )
+} )
 
-router.get('/:id/edit', (req, res, next) => {
-  let postID = req.params.id;
-  knex('posts').where('id', postID).first().then((post) => {
-    res.render('edit-post', {
-      post: post
-    })
-  })
-})
+router.get( '/:id/edit', authorizedUser, ( req, res, next ) => {
+    let postID = req.params.id;
+    knex( 'posts' ).where( 'id', postID ).first().then( ( post ) => {
+        res.render( 'edit-post', {
+            post: post
+        } )
+    } )
+} )
 
 router.post('/', authorizedUser, (req, res, next) => {
   knex('posts').insert({
