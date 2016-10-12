3a10336a101d81adf8d9ca96a5ad9cb3b5dc0692
Roberto  Varela <rvarela11@roberts-mbp.attlocal.net>
2016-10-12 18:30:40 -0500

    added comments/edit.ejs

--- test/users_route.test.js
+++ test/users_route.test.js
@@ -11,7 +11,7 @@ describe('GET /users', () => {
     request(app)
       .get('/logout')
       .end((err, res) => {
-        if(err){
+        if (err) {
           done(err);
         }
         expect.stats;
@@ -21,26 +21,27 @@ describe('GET /users', () => {
   });
 });
 
-describe('POSTS /users/1', function () {
-  xit('should show all of the posts', function (done) {
+describe('POSTS /users/1', function() {
+  xit('should show all of the posts', function(done) {
     request(app).get('/posts')
       .expect(200)
       .end((err, res) => {
-        if(err){
+        if (err) {
           done(err);
         }
 
       });
   });
 
-  xit('should show a single post with all of the comments', function (done) {
+  xit('should show a single post with all of the comments', function(done) {
     request(app).get('/posts/1')
       .expect(200)
       .end((err, res) => {
-        if(err){
+        if (err) {
           done(err);
         }
-        knex('posts').where('posts.id', 1).innerJoin('comments', 'posts.id', 'comments.post_id').then((post)=>{
+        knex('posts').where('posts.id', 1).innerJoin('comments',
+          'posts.id', 'comments.post_id').then((post) => {
           expect(res.text).to.contain(post[0].title);
           expect(res.text).to.contain(post[0].body);
           expect(res.text).to.contain(post[0].content);
@@ -48,15 +49,15 @@ describe('POSTS /users/1', function () {
         });
       });
   });
-  xit('should show a the edit single post page', function (done) {
+  xit('should show a the edit single post page', function(done) {
     request(app).get('/posts/1/edit')
-    .expect(200)
-    .end((err, res)=>{
-      if(err){
-        done(err);
-      }
-      expect(res.text).to.contain('</form>');
-    });
+      .expect(200)
+      .end((err, res) => {
+        if (err) {
+          done(err);
+        }
+        expect(res.text).to.contain('</form>');
+      });
 
   });
 });
