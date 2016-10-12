bbc0b16d3e67af755aee795ab6d9c55ea3efcf1f
Roberto  Varela <rvarela11@Roberts-MacBook-Pro.local>
2016-10-12 15:45:22 -0500

    added edit & delete for comments routes

--- server.js
+++ server.js
@@ -39,12 +39,14 @@ const checkAuth = function(req, res, next) {
 const users = require('./routes/users');
 const auth = require('./routes/auth');
 const posts = require('./routes/posts');
+const comments = require('./routes/comments');
 
 // Assign Routes to Server
 app.use('/auth', auth);
 // app.use('/logout', auth);
 app.use('/users', users);
 app.use('/posts', posts);
+app.use('/comments', comments);
 app.use('/', (req, res, next) => {
   res.render('./pages/index');
 });
