95522141592740b75b4c10110d5a078bdeba2868
Frank Gonzalez <frank.gonzalez628@gmail.com>
2016-10-12 15:29:00 -0500

    logout fix

--- server.js
+++ server.js
@@ -17,22 +17,22 @@ const ejs = require('ejs');
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({
-    extended: false
+  extended: false
 }));
 // app.use(bodyParser.json());
 app.use(cookieSession({
-    name: 'session',
-    keys: ['loggedIn', 'username'],
+  name: 'session',
+  keys: ['loggedIn', 'username'],
 }));
 app.use(methodOverride('_method'));
 
 //use as second argument whenever a user needs to be authenticated and logged in to view
 
 const checkAuth = function(req, res, next) {
-    if (!req.session.user) {
-        return res.sendStatus(401);
-    }
-    next();
+  if (!req.session.user) {
+    return res.sendStatus(401);
+  }
+  next();
 };
 
 // Declare routes variables
@@ -42,21 +42,19 @@ const posts = require('./routes/posts');
 
 // Assign Routes to Server
 app.use('/auth', auth);
-// app.use('/logout', auth);
 app.use('/users', users);
 app.use('/posts', posts);
 app.use('/', (req, res, next) => {
-    res.render('./pages/index');
+  res.render('./pages/index');
 });
 
 const port = process.env.PORT || 3000;
 // Server Listener
 app.listen(port, function() {
-    console.log(process.env.NODE_ENV, 'listening on port: ' + port);
+  console.log(process.env.NODE_ENV, 'listening on port: ' + port);
 });
 
 module.exports = app;
-
 
 
 
