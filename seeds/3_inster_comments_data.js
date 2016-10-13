6b6e49588483f26e79007f9aa26814f1be1e3bc0
Tosin <midastouchproductions@gmail.com>
2016-10-13 12:36:40 -0500

    fix seeds to number posts

--- seeds/3_inster_comments_data.js
+++ seeds/3_inster_comments_data.js
@@ -6,11 +6,11 @@ exports.seed = function(knex, Promise) {
       knex.raw('ALTER SEQUENCE comments_id_seq RESTART WITH 1').then();
       return Promise.all([
         // Inserts seed entries
-        knex('comments').insert({content: 'Booyah', post_id: 2, user_id: 1}),
-        knex('comments').insert({content: 'No way Jose', post_id: 1, user_id: 1}),
-        knex('comments').insert({content: 'Leave me alone', post_id: 3, user_id: 2}),
-        knex('comments').insert({content: 'Woo woo woo', post_id: 3, user_id: 3 }),
-        knex('comments').insert({content: 'Yahboo', post_id: 1, user_id: 3})
+        knex('comments').insert({content: '1 - Booyah', post_id: 2, user_id: 1}),
+        knex('comments').insert({content: '2 - No way Jose', post_id: 1, user_id: 1}),
+        knex('comments').insert({content: '3 - Leave me alone', post_id: 3, user_id: 2}),
+        knex('comments').insert({content: '4 - Woo woo woo', post_id: 3, user_id: 3 }),
+        knex('comments').insert({content: '5 - Yahboo', post_id: 1, user_id: 3})
       ]);
     });
 };
