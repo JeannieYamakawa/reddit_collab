d49c8aa5b20d9d772c42e9fc711e5f1b903d0bd7
Tosin <midastouchproductions@gmail.com>
2016-10-13 12:40:39 -0500

    fixes seeds once more

--- seeds/1_insert_users_data.js
+++ seeds/1_insert_users_data.js
@@ -6,8 +6,8 @@ exports.seed = function(knex, Promise) {
       knex.raw('ALTER SEQUENCE users_id_seq RESTART WITH 1').then();
       return Promise.all([
         // Inserts seed entries
-        knex('users').insert({username: 'joeySucks1', password: 'password', email: 'joey@gmail.com', admin: false }),
-        knex('users').insert({username: 'frankSucks2', password: 'password', email: 'frank@gmail.com', admin: false }),
+        knex('users').insert({username: 'joeySucks2', password: 'password', email: 'joey@gmail.com', admin: false }),
+        knex('users').insert({username: 'frankSucks1', password: 'password', email: 'frank@gmail.com', admin: false }),
         knex('users').insert({username: 'joeyRulez3', password: 'password', email: 'me@gmail.com', admin: true })
       ]);
     });
--- seeds/3_inster_comments_data.js
+++ seeds/3_inster_comments_data.js
@@ -6,8 +6,8 @@ exports.seed = function(knex, Promise) {
       knex.raw('ALTER SEQUENCE comments_id_seq RESTART WITH 1').then();
       return Promise.all([
         // Inserts seed entries
-        knex('comments').insert({content: '1 - Booyah', post_id: 2, user_id: 1}),
-        knex('comments').insert({content: '2 - No way Jose', post_id: 1, user_id: 1}),
+        knex('comments').insert({content: '2 - Booyah', post_id: 2, user_id: 1}),
+        knex('comments').insert({content: '1 - No way Jose', post_id: 1, user_id: 1}),
         knex('comments').insert({content: '3 - Leave me alone', post_id: 3, user_id: 2}),
         knex('comments').insert({content: '4 - Woo woo woo', post_id: 3, user_id: 3 }),
         knex('comments').insert({content: '5 - Yahboo', post_id: 1, user_id: 3})
